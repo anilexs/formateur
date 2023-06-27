918797817fdd4566979d2a704694ce84beb18447
alexis <alexissimoes94@gmail.com>
2023-06-27 16:34:24 +0200

    v1

---
+++ js/cour 1/js/structure.js
@@ -0,0 +1,63 @@
+// var largeur = 5;
+// var longueur = 10;
+// var hauteur = 7;
+
+// var volume = 0;
+
+// volume = largeur*longueur*hauteur;
+// console.log(volume);
+
+// let age = 2;
+// if (age<5){
+//     console.log("bebe");
+// }else if(age>=5 && age<=10){
+//     console.log("enfant");
+// }else if( age>=11 && age<=18){
+//     console.log("ado");
+// }else if (age > 18){
+//     console.log("vieux");
+// }
+
+// var num = 0;
+// switch (num){
+//     case 1:
+//         console.log("lundie");
+//     break;
+//     case 2:
+//         console.log("mardi");
+//     break;
+//     case 3:
+//         console.log("mercredi");
+//     break;
+//     case 4:
+//         console.log("jeudi");
+//     break;
+//     case 5:
+//         console.log("vendredi");
+//     break;
+//     case 6:
+//         console.log("samedi");
+//     break;
+//     case 0:
+//         console.log("dimanche");
+//     break;
+// }
+
+// var age = 16;
+// var ternaire = (age <= 18) ? "mineur" : "adulte";
+// console.log(ternaire);
+
+// var ans = 2003;
+// if (ans%4 == 0 && ans%100 !== 0){
+//     console.log("bisextile");
+// }else if (ans%400 == 0){
+//     console.log("bisextille");
+// }else{
+//     console.log("non bisextille");
+// }
+var ans2 = 2003;
+if (ans2%4 == 0 && ans2%100 !== 0 ||ans2%400 == 0){
+    console.log("bisextile");
+}else{
+    console.log("non bisextille");
+}
