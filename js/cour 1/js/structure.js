fad61e7267bd65507af88fac7b1d4d897577c51d
alexis <alexissimoes94@gmail.com>
2023-06-28 16:40:58 +0200

    v2

--- js/cour 1/js/structure.js
+++ js/cour 1/js/structure.js
@@ -55,9 +55,70 @@
 // }else{
 //     console.log("non bisextille");
 // }
-var ans2 = 2003;
-if (ans2%4 == 0 && ans2%100 !== 0 ||ans2%400 == 0){
-    console.log("bisextile");
-}else{
-    console.log("non bisextille");
+
+// var ans2 = 2003;
+// if (ans2%4 == 0 && ans2%100 !== 0 ||ans2%400 == 0){
+//     console.log("bisextile");
+// }else{
+//     console.log("non bisextille");
+// }
+
+// let fruit = ["bannane", "pomme", "mangue"]
+// fruit.splice(2,0,"fraise")
+// console.log(fruit);
+// fruit.fill("goyave")
+// console.log(fruit);
+
+// function maxMin (a,b, c = true){
+//     if (c == true){
+//         if (a > b){
+//             console.log(a);
+//             return a;
+//         }else{
+//             console.log(b);
+//             return b;
+//         }
+//     }else if(c == false){
+//         if (a < b){
+//             console.log(a);
+//             return a;
+//         }else{
+//             console.log(b);
+//             return b;
+//         }
+//     }
+
+// }
+
+// maxMin(4,1,false);
+
+// EX a fair :
+
+// a est retourné si c = true et a > b ou bien c = false et a < b
+// 	=> a
+// b est retourné s c = true et b > a ou bien c  = false et b < a
+// 	=> b
+    // function maxMin(a, b, c = true) {
+    //     if (((c == true) && (a > b)) || ((c == false) && (a < b))) {
+    //         return a;
+    //     } else if (((c == true) && (a < b)) || ((c == false) && (a > b))) {
+    //         return b;
+    //     }
+    // }
+        
+    // let result = maxMin(3, 8, false);
+    // console.log(result);
+// 
+
+var tableauFun = [1,2,3,4,5,6,7,8,9] 
+function sommeTab(tableau){
+    var num = 0;
+    for(var i = 0; i < tableau.length; i++){
+        num = num + tableauFun[i];
+    }
+    return num;
 }
+
+var resulta = sommeTab(tableauFun);
+console.log(resulta);
+
