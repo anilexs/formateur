497759d819381b4c0e9ea3b146ec76980e8f4412
alexis <alexissimoes94@gmail.com>
2023-07-07 16:50:28 +0200

    v13

--- js/cour 1/Jquery/js/NombreMister.js
+++ js/cour 1/Jquery/js/NombreMister.js
@@ -120,33 +120,160 @@ var clickCount = 0;
 var firstClickTime = 0;
 var resetTimeout;
 var egg1ActionNone = $('.egg1ActionNone');
+var timerInput = $('#timer');
+var timerInterval;
+var remainingTime;
+var codeInput = $('#codeInput').val();
 
 egg1.on("click", function() {
   if (clickCount === 0) {
     firstClickTime = new Date().getTime();
+    updateTimerDisplay(120000); // Afficher 2:00 au départ (2 minutes en millisecondes)
   }
-  
+
   clickCount++;
   console.log(clickCount);
   if (clickCount === 5 && new Date().getTime() - firstClickTime < 2000) {
-    //  action
+    // Action
     egg1ActionNone.removeClass('egg1ActionNone');
     egg1ActionNone.addClass('egg1Div');
-    
-    // fin action 
-    clearTimeout(resetTimeout); 
+
+    // Fin de l'action
+    clearTimeout(resetTimeout);
     resetTimeout = setTimeout(function() {
-      clickCount = 0; 
-    }, 2000); 
+      clickCount = 0;
+      egg1ActionNone.removeClass('egg1Div');
+      egg1ActionNone.addClass('egg1ActionNone');
+      clearInterval(timerInterval); // Mettre en pause le chronomètre
+    }, 60000); // Compte à rebours d'une minute (60000 millisecondes)
+
+    startTimer(120000); // Démarrer le chronomètre à 2 minutes (120000 millisecondes)
+
   } else if (clickCount === 5 || new Date().getTime() - firstClickTime >= 2000) {
     clearTimeout(resetTimeout);
     clickCount = 0;
+    updateTimerDisplay(120000); // Réinitialiser le chronomètre à 2 minutes (120000 millisecondes)
+    clearInterval(timerInterval); // Mettre en pause le chronomètre
   }
 });
+
+$('#startButton').click(function() {
+  var codeInputValue = $('#codeInput').val();
+
+  if (codeInputValue === '123') {
+    clearInterval(timerInterval); // Mettre en pause le chronomètre
+    // Autres actions à effectuer lorsque le code est valide
+
+  } else {
+    valu = '';
+    $('#codeInput').val('');
+  }
+});
+
+var val1 = $('#val1')
+var val2 = $('#val2')
+var val3 = $('#val3')
+var val4 = $('#val4')
+var val5 = $('#val5')
+var val6 = $('#val6')
+var val7 = $('#val7')
+var val8 = $('#val8')
+var val9 = $('#val9')
+var valC = $('#valC')
+var val0 = $('#val0')
+var valD = $('#valD')
+var valu = '';
+
+$(valC).click(() =>{
+    valu = '';
+    $('#codeInput').val(valu);
+})
+$(val1).click(() =>{
+    valu = valu + "1";
+    $('#codeInput').val(valu);
+})
+$(val2).click(() =>{
+    valu = valu + "2";
+    $('#codeInput').val(valu);
+})
+$(val3).click(() =>{
+    valu = valu + "3";
+    $('#codeInput').val(valu);
+})
+$(val4).click(() =>{
+    valu = valu + "4";
+    $('#codeInput').val(valu);
+})
+$(val5).click(() =>{
+    valu = valu + "5";
+    $('#codeInput').val(valu);
+})
+$(val6).click(() =>{
+    valu = valu + "6";
+    $('#codeInput').val(valu);
+})
+$(val7).click(() =>{
+    valu = valu + "7";
+    $('#codeInput').val(valu);
+})
+$(val8).click(() =>{
+    valu = valu + "8";
+    $('#codeInput').val(valu);
+})
+$(val9).click(() =>{
+    valu = valu + "9";
+    $('#codeInput').val(valu);
+})
+$(val0).click(() =>{
+    valu = valu + '0';
+    $('#codeInput').val(valu);
+})
+$(valD).click(() =>{
+    valu = valu + "#";
+    $('#codeInput').val(valu);
+})
+
+
+function startTimer(duration) {
+  var startTime = new Date().getTime();
+  var endTime = startTime + duration;
+  remainingTime = duration;
+
+  timerInterval = setInterval(function() {
+    var currentTime = new Date().getTime();
+    remainingTime = endTime - currentTime;
+
+    if (remainingTime <= 0) {
+      clearInterval(timerInterval);
+      updateTimerDisplay(0);
+      // Action à la fin du chronomètre
+      egg1ActionNone.removeClass('egg1Div');
+        egg1ActionNone.addClass('egg1ActionNone');
+    } else {
+      updateTimerDisplay(remainingTime);
+    }
+  }, 1000); // Mettre à jour le chronomètre toutes les 1 seconde
+}
+
+function updateTimerDisplay(time) {
+  var minutes = Math.floor(time / 60000);
+  var seconds = Math.floor((time % 60000) / 1000);
+
+  var minutesString = padZero(minutes);
+  var secondsString = padZero(seconds);
+
+  timerInput.val(minutesString + ':' + secondsString);
+}
+
+function padZero(number) {
+  return (number < 10 ? '0' : '') + number;
+}
+
 
 
 
 $(btnXegg1).click(() => {
     egg1ActionNone.removeClass('egg1Div');
     egg1ActionNone.addClass('egg1ActionNone');
+    clearInterval(timerInterval); // Mettre en pause le chronomètre
 });
